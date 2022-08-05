4998c3289f62ac1f5d144e6837232963931e3159
ogait-ashtar <ogait.desenvolvedor.jr@gmail.com>
2022-08-04 22:17:25 -0300

    chore: swagger and refactor index with route

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const routes = require('./src/routes/paleta.route');
 const connectToDatabase = require('./src/database/database');
 
 const port = process.env.PORT || 3001;
@@ -10,6 +11,7 @@ connectToDatabase();
 
 app.use(express.json());
 app.use(cors());
+app.use('/bandas', routes);
 
 
 app.listen(port, () => {
--- src/routes/banda.route.js
+++ src/routes/banda.route.js
@@ -1,15 +1,15 @@
 const router = require('express').Router();
 const controllerBandas = require('../controllers/banda.controller');
-// const swaggerUi = require('swagger-ui-express');
-// const swaggerDocument = require('../../swagger.json');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../../swagger.json');
 
 const {
   validId,
   validObjectBody,
 } = require('../middlewares/banda.middleware');
 
-// router.use('/api-docs', swaggerUi.serve);
-// router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 router.get('/all-bandas', controllerBandas.findAllBandasController);
 router.get(
